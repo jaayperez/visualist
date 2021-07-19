613a6c11b1d684386dfb7912c590a7971d5e8cac
jaayperez <justin@startrev.com>
2021-07-19 02:44:50 -0700

    Add the Alan button to the app and add voice commands

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import alanBtn from "@alan-ai/alan-sdk-web";
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    // Adding the Alan button
+	useEffect(() => {
+		alanBtn({
+      key: '1f4ec1145384064b6dfca9f6aa11f4b72e956eca572e1d8b807a3e2338fdd0dc/stage',
+			onCommand: (commandData) => {
+				if (commandData.command === 'go:back') {
+					// Call the client code that will react to the received command
+				}
+			}
+		});
+	}, []);
   return (
     <div className="App">
       <header className="App-header">
